3fdccdde92dd9dbdbf117371004e4b71da52da73
Henrry-Maximo <henrrylimadasilva@gmail.com>
2025-06-15 15:35:31 -0300

    📚  Docs: Add comments at codebase

--- C/estruturas_de_dados/conversion_temperature.c
+++ C/estruturas_de_dados/conversion_temperature.c
@@ -11,7 +11,23 @@ float converte (float c)
 	return f;
 }
 
+/*
+	No programa, é preciso ter a função principal (main),
+	pois a execução de um programa começa por ela.
+	A função `main` é automaticamente chamada quando o programa
+	é carregado para a memória.
+	As funções auxiliares são chamadas direta ou indiretamente,
+	pela função principal.
+	
+	Também é necessário reservar uma área na memória para armazenar
+	determinado dado, usando a declaração de variáveis, também
+	dizendo o tipo de dado armazenado naquela posição de memória.
+*/
+
 int main (void) {
+	// A seguinte declaração reserva um espaço de memória para 
+	// armazenar um valor real (ponto flutuante). Esse espaço
+	// de memória é referênciado pelo símbolo t1.
 	float t1;
 	float t2;
 	
@@ -28,3 +44,37 @@ int main (void) {
 	printf("Temperature at fahrenheit: %f\n", t2);
 	return 0;
 }
+
+/*
+	formatação: é possível escrever vários comandos
+	em uma única linha ou dividir um mesmo comando
+	em diversas linhas. Para melhor clareza, optamos
+	por escrever cada comando em uma linha.
+	
+	funções: o programa é constituído de funções pequnas.
+	independentes entre si. Possuindo dois ambientes:
+		Global: externo às funções
+		Local: definidos pelas diversas funções, independentes
+		entre si.
+		
+		
+	tempo de vida e visibilidade das variáveis:
+		Uma variável loca, declarada dentro de uma função, só
+		"vive" enquanto a função estiver sendo executada, e 
+		nenhuma outra função possuí acesso direto a ela. 
+		Também precisam ser explicitamente inicializadas antes
+		do uso, caso contrário carregarão "lixo", isto é, valores
+		indefinidos.
+	alternativa: é possível definir variáveis externas às funções
+	que podem ser acessadas pelo nome, por qualquer função subsequente
+	no programa. Também armazenam os valores de forma permanente, pelo
+	menos enquanto o programa estiver sendo executado. Ou seja, elas
+	retêm seus valores mesmo quando as funções que as acessam são
+	finalizadas. É possível definir variáveis globais em qualquer 
+	parte do programa, mas é prática comum, defini-las no início
+	do arquivo-fonte.
+	cuidado: sempre que possível, evite o uso de variáveis globais.
+	Por razões de clareza e estruturação, é recomendado o uso de 
+	variáveis locais sempre que possível.
+
+*/
